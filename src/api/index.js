3872469f7a271983858e1690a4ace253b0cb7c8d
Istvan Jano <janoist1@gmail.com>
2017-08-09 09:31:01 +0100

    Implement service updates API

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+const API_URL = 'http://localhost:3001/data';
+
+export const fetchServiceUpdates = () => fetch(API_URL)
+  .then(response => new Promise(resolve =>
+    setTimeout(() => resolve(response), 1000)
+  ))
+  .then(response => response.json());
---
+++ src/api/index.test.js
@@ -0,0 +1,26 @@
+import '../../setupJest'; // TODO: find out why it isn't get picked up from npm config
+import { fetchServiceUpdates } from '../api';
+
+
+describe('api::fetchServiceUpdates', () => {
+  const mockData = { test: 'test' };
+  let setTimeout;
+
+  beforeEach(() => {
+    setTimeout = global.setTimeout;
+    global.setTimeout = fn => fn();
+  });
+
+  afterEach(() => {
+    global.setTimeout = setTimeout;
+  });
+
+  it('should call fetch and return json', () => {
+    fetch.mockResponse(JSON.stringify(mockData));
+
+    return fetchServiceUpdates().then(data => {
+      expect(data).toBeDefined();
+      expect(data).toEqual(mockData);
+    });
+  });
+});
