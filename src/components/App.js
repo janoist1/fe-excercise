53e239c42f0c229d8b6c53928bf886cc938dd41b
Istvan Jano <janoist1@gmail.com>
2017-08-09 09:31:57 +0100

    Implement initial App component

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { fetchServiceUpdates } from '../api'
+import './App.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    return fetchServiceUpdates().then(data => {
+      this.setState({ data });
+    });
+  }
+
+  renderLoading() {
+    return (
+      <div>Loading ...</div>
+    );
+  }
+
+  render() {
+    const { data } = this.state;
+
+    if (!data) {
+      return this.renderLoading();
+    }
+
+    const { callingPoints } = data;
+
+    return (
+      <div className="App">
+        <header className="App__header">
+          <h3 className="App__from">{callingPoints[0].station}</h3>
+          <h3 className="App__to"><span>to</span> {callingPoints.slice(-1)[0].station}</h3>
+          <span className="App__operated-by">Operated by Thameslink</span>
+        </header>
+        <main>
+
+        </main>
+        <footer>by janoist1@gmail.com</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
